edcf3cabce7388c8ef85f58d35e2d6b3ed1d630b
fvrolijken <friso.vrolijken@qiy.nl>
2016-11-04 09:44:22 +0100

    ...

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,15 @@
 node {
     // Mark the code checkout 'stage'....
-//    stage 'Checkout'
+    stage 'Checkout'
  
     // Get some code from a GitHub repository
-//    git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
+    git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
  
     // Get the maven tool.
     // ** NOTE: This 'M3' maven tool must be configured
     // **       in the global configuration.
 
-    def mvnHome = tool 'M3'
+    def mvnHome = tool 'maven'
  
     // Mark the code build 'stage'....
     stage 'Build'
