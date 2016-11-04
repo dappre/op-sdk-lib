bdc3f883ec0fb7aae719ac4de906535017b925cd
fvrolijken <friso.vrolijken@qiy.nl>
2016-11-04 12:21:55 +0100

    ...

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,10 +16,10 @@ node {
             // Get some code from a GitHub repository
 //            git url: 'https://github.com/digital-me/${project}.git'
             
-            stage 'Find new version'
-            echo "Current version = ${findVersion(project)}"
-            echo "GIT URL = ${env.GIT_URL}"
-         
+            stage 'Find new version' {
+                echo "Current version = ${findVersion(project)}"
+                echo "GIT URL = ${env.GIT_URL}"
+            }
             // Mark the code build 'stage'....
             stage 'Build'
             // Run the maven build
@@ -34,34 +34,39 @@ node {
 }
 
 def findVersion(project) {
-    echo "in find version ${project}"
-    def url1 = "https://api.github.com/repos/digital-me/${project}/tags".toURL()
-    def tagPrefix = "rel-"
-    def min = new Tuple(0,0,12) // for bootstrapping
-    def json = new groovy.json.JsonSlurper().parseText(url1.text);
-    return json.collect {
-        echo it
-        def matcher = ( it.name =~ /${tagPrefix}([0-9]+).([0-9]+).([0-9]+)$/ )
-        if (!matcher.find()) {
-            // always have somthing in the result list,
-            // if there are no items in the list, max will fail
-            return min;
-        }
-        // else
-        return new Tuple(
-            Integer.parseInt(matcher.group(1)),
-            Integer.parseInt(matcher.group(2)),
-            Integer.parseInt(matcher.group(3)))
-    }
-    .plus(min)
-    .max {t1, t2 ->
-        if (t1[0].compareTo(t2[0]) == 0){
-            if (t1[1].compareTo(t2[1]) == 0) {
-                return t1[2].compareTo(t2[2]);
+    try {
+        echo "in find version ${project}"
+        def url1 = "https://api.github.com/repos/digital-me/${project}/tags".toURL()
+        def tagPrefix = "rel-"
+        def min = new Tuple(0,0,12) // for bootstrapping
+        def json = new groovy.json.JsonSlurper().parseText(url1.text);
+        return json.collect {
+            echo it
+            def matcher = ( it.name =~ /${tagPrefix}([0-9]+).([0-9]+).([0-9]+)$/ )
+            if (!matcher.find()) {
+                // always have somthing in the result list,
+                // if there are no items in the list, max will fail
+                return min;
             }
-            return t1[1].compareTo(t2[1]);
+            // else
+            return new Tuple(
+                Integer.parseInt(matcher.group(1)),
+                Integer.parseInt(matcher.group(2)),
+                Integer.parseInt(matcher.group(3)))
         }
-        return t[0].compareTo(t2[0]);
+        .plus(min)
+        .max {t1, t2 ->
+            if (t1[0].compareTo(t2[0]) == 0){
+                if (t1[1].compareTo(t2[1]) == 0) {
+                    return t1[2].compareTo(t2[2]);
+                }
+                return t1[1].compareTo(t2[1]);
+            }
+            return t[0].compareTo(t2[0]);
+        }
+    } catch (e) {
+        echo e;
+        return "0.0.14";
     }
 }
 
