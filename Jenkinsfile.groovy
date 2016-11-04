bed5b3e2f9d09571ecc3358c17132bd612c30886
fvrolijken <friso.vrolijken@qiy.nl>
2016-11-04 12:23:30 +0100

    ...

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ def branch='master'
 def release=false
 def project='op-sdk-lib'
 
-echo "Build URL=${env.BUILD_URL}"
+echo "GIT URL=${env.GIT_URL}"
 
 node {
     try {
@@ -16,10 +16,9 @@ node {
             // Get some code from a GitHub repository
 //            git url: 'https://github.com/digital-me/${project}.git'
             
-            stage 'Find new version' {
-                echo "Current version = ${findVersion(project)}"
-                echo "GIT URL = ${env.GIT_URL}"
-            }
+            stage 'Find new version' 
+            echo "Current version = ${findVersion(project)}"
+            
             // Mark the code build 'stage'....
             stage 'Build'
             // Run the maven build
