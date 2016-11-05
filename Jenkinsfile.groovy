a03489e0e1572ff50e9781e946c7967d68983c6a
fvrolijken <friso.vrolijken@qiy.nl>
2016-11-05 13:34:32 +0100

    success!

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,25 +7,24 @@ def project='op-sdk-lib'
 def tagPrefix='rel-'
 
 node {
-    def artifactoryMaven = null
+    def artifactoryMaven=null
+    def newVersion=null
 
-//    try {
+    try {
         withEnv(["PATH+MAVEN=${tool 'maven'}/bin", "JAVA_HOME=${tool 'jdk1.8.0_latest'}"]) {
-            stage('Clean') {
+            stage('Get clean source') {
+                deleteDir()
+                git credentialsId: '200c2dab-036b-48a3-a824-1f4257be94ff', url: 'https://github.com/digital-me/op-sdk-lib.git'
                 sh "mvn clean"
             }
             
             stage ('Set new version') {
-                def currVersion=sh script: 'tmp=\$(git tag -l  "rel-*" | cut -d\'-\' -f2- | sort -r -V | head -n1);echo \${tmp:-\'rel-0.0.12\'}', returnStdout: true
-                def newVersion = newVersion(tagPrefix, update, currVersion);
+                def currVersion=sh (script: 'tmp=\$(git tag -l  "rel-*" | cut -d\'-\' -f2- | sort -r -V | head -n1);echo \${tmp:-\'rel-0.0.12\'}', returnStdout: true).trim()
+                newVersion = nextVersion(tagPrefix, update, currVersion);
                 echo "current version is ${currVersion}, new version will be ${newVersion}"
                 sh "mvn -DnewVersion=$newVersion versions:set"
             }
             
-//            stage('Build') {
-//                sh "mvn -Dmaven.test.failure.ignore install"
-//            }
-
             stage('Prepare Artifactory') {
                 def server = Artifactory.server('qiy-artifactory@boxtel')
                 artifactoryMaven = Artifactory.newMavenBuild()
@@ -38,17 +37,34 @@ node {
                 def buildInfo = Artifactory.newBuildInfo()
                 artifactoryMaven.run pom: 'pom.xml', goals: 'clean install', buildInfo: buildInfo
             }
+
+            stage('Tag release') {   
+                sh "git tag -a 'rel-${newVersion}' -m 'Release tag by Jenkins'"
+                sshagent(['200c2dab-036b-48a3-a824-1f4257be94ff']) {
+//                withCredentials([[$class: 'UsernamePasswordMultiBinding', 
+//                    credentialsId: '200c2dab-036b-48a3-a824-1f4257be94ff', 
+//                    usernameVariable: 'GIT_USERNAME', 
+//                    passwordVariable: 'GIT_PASSWORD']]) {
+                    sh "git remote set-url origin 'git@github.com:digital-me/op-sdk-lib.git'"
+                    sh "git -c core.askpass=true push origin 'rel-${newVersion}'"
+                }
+            }
+            
+            stage('Start next') {
+               // build job: 'build', parameters: [[$class: 'StringParameterValue', name: 'target', value: target], [$class: 'ListSubversionTagsParameterValue', name: 'release', tag: release], [$class: 'BooleanParameterValue', name: 'update_composer', value: update_composer]]
+                
+            }
         }
-//     } catch (e) {
-//         // TODO [FV 20161104] Send XMPP message here
-//         throw e;
-//     } finally {
-//         step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-//     }
+     } catch (e) {
+         // TODO [FV 20161104] Send XMPP message here
+         throw e;
+     } finally {
+         step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+     }
 }
 
 @NonCPS
-def newVersion(tagPrefix, update, currVersion) {
+def nextVersion(tagPrefix, update, currVersion) {
 //    println "${tagPrefix} - ${update} - ${currVersion}"
     if (currVersion.length() < tagPrefix.length() + 5)  {
         throw new IllegalArgumentException("${currVersion} is too short for prefix ${tagPrefix}")
